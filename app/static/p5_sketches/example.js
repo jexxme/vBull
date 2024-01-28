66edfa9b500900ca59572c78286726dd5eced968
Tim Billig <tim.billig2@gmail.com>
2024-01-28 20:12:53 +0100

    Feat: Included p5.js + added example sketch to start off of

---
+++ app/static/p5_sketches/example.js
@@ -0,0 +1,36 @@
+function rsetup() {
+    // Drawn once
+    createCanvas(500, 400); // Canvas size
+    background(20); // Background color
+    
+    // Settings
+    const trend_intenseness = 4.0
+    const step = 10;
+    const steps = 48;
+    
+    // Computation values
+    let trend = 10; // Initial trend (positive is down, negative is up)
+    let x = 10;
+    
+    
+    let lastY = 200; // Initial stock value
+    
+    for (i = 0; i < steps; i++) {
+      if (trend < 0) {
+        // Positive trend (green)
+        stroke(0, 255, 60);
+      } else {
+        // Negative trend (red)
+        stroke(255, 30, 60);
+      }
+      line(x, lastY, x + step, lastY + trend);
+      x += step;
+      lastY += trend;
+      trend = random(-10.0 * trend_intenseness, 10.0 * trend_intenseness);
+    }
+  }
+  
+  function draw() {
+    // Drawn per frame
+    
+  }
